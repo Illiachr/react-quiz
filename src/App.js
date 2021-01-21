9d8bd4975ead132e6709ac3faa3ed8de9f4a135b
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-01-21 20:41:15 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Layout from './hoc/Layout/Layout'
+import { Route, Switch} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
+import QuizList from './containers/QuizList/QuizList'
+import Auth from './containers/Auth/Auth'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path="/quiz" component={Quiz} />
+        <Route path="/quiz-creator" component={QuizCreator} />
+        <Route path="/quiz/:id" component={Auth} />
+        <Route path="/" component={QuizList} />
+      </Switch>
     </Layout>
   );
 }
