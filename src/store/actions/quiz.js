5a121c6003e78f90957b59b35833edf32c51a122
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-01-25 22:57:02 +0200

    modify container Quiz with redux

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -1,5 +1,14 @@
 import axios from "../../axios/axios-quiz"
-import { FETCH_QUIZES_ERORR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS } from "./actionTypes"
+import { 
+            FETCH_QUIZES_ERORR,
+            FETCH_QUIZES_START,
+            FETCH_QUIZES_SUCCESS,
+            FETCH_QUIZ_SUCCESS,
+            FINISH_QUIZ,
+            QUIZ_SET_STATE,
+            QUIZ_NEXT_QUESTION,
+            QUIZ_RETRY,
+        } from "./actionTypes"
 
 export function fetchQuizes() {
     return async dispatch => {
@@ -23,6 +32,18 @@ export function fetchQuizes() {
     }
 }
 
+export function fetchQuizById(quizId) {
+    return async dispatch => {
+        dispatch(fetchQuizesStart())
+        try {
+            const response = await axios.get(`/quizDb/${quizId}.json`)
+            const quiz = response.data
+            
+            dispatch(fetchQuizSuccess(quiz))
+        } catch (e) { dispatch(fetchQuizesErr(e)) }
+    }
+}
+
 export function fetchQuizesStart() {
     return {
         type: FETCH_QUIZES_START
@@ -36,9 +57,81 @@ export function fetchQuizesSuccess(quizes) {
     }
 }
 
+export function fetchQuizSuccess(quiz) {
+    return {
+        type: FETCH_QUIZ_SUCCESS,
+        quiz
+    }
+}
+
 export function fetchQuizesErr(e) {
     return {
         type: FETCH_QUIZES_ERORR,
         error: e
     }
+}
+
+export function quizSetState(answerState, results) {
+    return {
+        type: QUIZ_SET_STATE,
+        answerState,
+        results
+    }
+}
+
+export function finishQuiz() {
+    return {
+        type: FINISH_QUIZ
+    }
+}
+
+export function quizNextQuestion(num) {
+    return {
+        type: QUIZ_NEXT_QUESTION,
+        activeQuestion: num
+    }
+}
+
+export function retryQuiz() {
+    return {
+        type: QUIZ_RETRY
+    }
+}
+
+export function quizAnswerQClick(answerId) {
+    return (dispatch, getState) => {
+        const state = getState().quiz
+        if (state.answerState) {
+            const key = Object.keys(state.answerState)[0]
+            if (state.answerState[key] === 'success') {
+                return;
+            }
+        }
+        const question = state.quiz[state.activeQuestion]
+        const results = state.results;
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
+            dispatch(quizSetState({[answerId]: 'success'}, results))
+            
+        } else {
+            results[question.id] = 'err'
+            dispatch(quizSetState({[answerId]: 'err'}, results))
+        } // end if question
+        const timeout = setTimeout(() => {
+            if (isQuizFinished(state)) {
+                dispatch(finishQuiz())
+            } else {
+                dispatch(quizNextQuestion(state.activeQuestion + 1))
+            } // end if isQuizFinished
+
+            clearTimeout(timeout)
+        }, 1000)
+    } // end return
+} // end onAnswerClickHandler
+
+function isQuizFinished(state) {
+    return state.activeQuestion + 1 === state.quiz.length
 }
--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,9 +1,23 @@
-import { FETCH_QUIZES_ERORR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS } from "../actions/actionTypes"
+import { 
+            FETCH_QUIZES_ERORR,
+            FETCH_QUIZES_START,
+            FETCH_QUIZES_SUCCESS,
+            FETCH_QUIZ_SUCCESS,
+            FINISH_QUIZ,
+            QUIZ_NEXT_QUESTION,
+            QUIZ_RETRY,
+            QUIZ_SET_STATE,
+        } from "../actions/actionTypes"
 
 const initialState = {
     quizDb: [],
     loading: false,
-    error: null
+    error: null,
+    results: {}, // { id: 'success' or 'error'} for All questions
+    isFinished: false,
+    activeQuestion: 0,
+    answerState: null, // { id: 'success' or 'error'}
+    quiz: null,
 }
 
 export default function quizReducer (state = initialState, action) {
@@ -18,13 +32,43 @@ export default function quizReducer (state = initialState, action) {
                 loading: false,
                 quizDb: action.quizDb
             }
+        case FETCH_QUIZ_SUCCESS:
+            return {
+                ...state, 
+                loading: false,
+                quiz: action.quiz
+            }
         case FETCH_QUIZES_ERORR:
             return {
                 ...state, 
                 loading: false,
                 error: action.erorr
+            }        
+        case QUIZ_SET_STATE:
+            return {
+                ...state, 
+                answerState: action.answerState,
+                results: action.results
             }
-        
+        case FINISH_QUIZ:
+            return {
+                ...state,
+                isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state,
+                activeQuestion: action.activeQuestion,
+                answerState: null
+            }
+        case QUIZ_RETRY:
+            return {
+                ...state,
+                results: {},
+                isFinished: false,
+                activeQuestion: 0,
+                answerState: null
+            }
         default:
             return state
     }
