8ba7ec00cc5fc033d6eaa5bf67142271f1be5788
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-01-27 11:25:50 +0200

    todo logout n autologin

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
-import Input from '../../components/UI/Input/Input';
-import axios from 'axios'
-import { connect } from 'react-redux';
-import { auth } from '../../store/actions/auth';
+import Input from '../../components/UI/Input/Input'
+import { connect } from 'react-redux'
+import { auth } from '../../store/actions/auth'
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -43,33 +42,20 @@ class Auth extends Component {
         }
     }
 
-    signInHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-
-        try {
-            const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAAUYCrXubYm18gb93tIN0GFAy8wqGegN8', authData)
-
-            console.log(res.data);
-        } catch (e) { console.warn(e); }
+    signInHandler = () => {
+        this.props.auth ( 
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            true
+        )
     } // end signInHandler
 
     signUpHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-
-        try {
-            const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAAUYCrXubYm18gb93tIN0GFAy8wqGegN8', authData)
-
-            console.log(res.data);
-        } catch (e) { console.warn(e); }
-        
+        this.props.auth ( 
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false
+        )        
     } // end signUpHandler
 
     submitHandler = e =>  e.preventDefault()
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,53 @@
-export function auth(email, pwd, isLogin) {
+import axios from "axios";
+import { USER_AUTH_SUCCESS, AUTH_LOGOUT } from "./actionTypes";
+
+
+export function auth(email, password, isLogin) {
+    return async dispatch => {
+        const authData = {
+            email,
+            password,
+            returnSecureToken: true
+        }
+        const postUrl = isLogin
+                        ? 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAAUYCrXubYm18gb93tIN0GFAy8wqGegN8'
+                        : 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAAUYCrXubYm18gb93tIN0GFAy8wqGegN8'
+
+        try {
+            const res = await axios.post(postUrl, authData)
+            const userData = {
+                    idToken: res.data.idToken,
+                    localId: res.data.localId,
+                    expiresIn: res.data.expiresIn * 1000,
+                    expTime: new Date(new Date().getTime() + this.expiresIn)
+                }
+
+            localStorage.userData = JSON.stringify(userData)
+            dispatch(authSuccess(userData.idToken))
+            dispatch(userLogout(userData.expiresIn))
+        } catch (e) { console.warn(e); }
+    }
+}
+
+export function authSuccess(tokenId) {
+    console.log(tokenId);
+    return {
+        type: USER_AUTH_SUCCESS,
+        token: tokenId
+    }
+}
+
+export function userLogout(time) {
     return dispatch => {
-        
+        setTimeout(() => {
+            dispatch(logout())
+        }, time)
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('userData')
+    return {
+        type: AUTH_LOGOUT
     }
 }
